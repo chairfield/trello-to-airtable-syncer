66806942dea590a14083acb01afaaf09de3143c6
Chris Hairfield <chris@miraclemessages.org>
2019-08-31 10:24:08 -0600

    Added second test, which depends on a new Record fake

--- src/clientLookupService.test.js
+++ src/clientLookupService.test.js
@@ -1,9 +1,34 @@
 const ClientLookupService = require('./clientLookupService');
 
+class Record {
+    constructor(id, clientName) {
+        this._id = id;
+        this._clientName = clientName;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    get(fieldName) {
+        if (fieldName === "Client Name") {
+            return this._clientName;
+        } else {
+            throw new Error("Unsupported");
+        }
+    }
+}
+
 describe("ClientLookupService -> filtering records to a match", () => {
-    test("When it is passed 0 records, an error is thrown", () => {
+    test("When passed 0 records, an error is thrown", () => {
         expect(() => {
             new ClientLookupService()._filterMatch([]);
         }).toThrow("No records returned.");
+    });
+
+    test("When passed 1 record, that record is returned", () => {
+        const record = new Record(123, "Joe");
+        const result = new ClientLookupService()._filterMatch([record]);
+        expect(result).toBe(record);
     });
 });
