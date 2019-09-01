e46b4995af202cf90fa79dfab4f1f4a86c19ed12
Chris Hairfield <chris@miraclemessages.org>
2019-08-31 20:49:39 -0600

    Added rolling file logs on a daily schedule

--- config/winston.js
+++ config/winston.js
@@ -1,23 +1,25 @@
 var appRoot = require('app-root-path');
 var winston = require('winston');
+require('winston-daily-rotate-file');
 
 var options = {
     errors: {
         level: 'error',
-        filename: `${appRoot}/logs/errors.log`,
+        filename: `${appRoot}/logs/%DATE%-error.log`,
+        datePattern: 'YYYY-MM-DD',
         handleExceptions: true,
         json: true,
         maxsize: 5242880, // 5MB
-        maxFiles: 5,
         colorize: false,
     },
     combined: {
-        level: 'info',
-        filename: `${appRoot}/logs/combined.log`,
+        level: 'debug',
+        filename: `${appRoot}/logs/%DATE%-combined.log`,
+        datePattern: 'YYYY-MM-DD',
+        zippedArchive: true,
         handleExceptions: true,
         json: true,
         maxsize: 5242880, // 5MB
-        maxFiles: 5,
         colorize: false,
     },
     console: {
@@ -30,8 +32,8 @@ var options = {
 
 var logger = winston.createLogger({
     transports: [
-        new winston.transports.File(options.errors),
-        new winston.transports.File(options.combined),
+        new winston.transports.DailyRotateFile(options.errors),
+        new winston.transports.DailyRotateFile(options.combined),
         new winston.transports.Console(options.console)
     ],
     exitOnError: false, // do not exit on handled exceptions
